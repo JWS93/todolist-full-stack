763d765ec31c4099b76132b1b949a775082ac5e1
JWS93 <justinscott820@gmail.com>
2025-03-15 20:17:35 -0700

    fixed render functionality

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -8,24 +8,13 @@ import {
   taskComplete
 } from "./requests.js";
 
-indexTasks(function (response) {
-  var htmlString = response.tasks.map(function(task) {
-    return "<div class='row mb-3 p-2 border rounded task-list'>" + 
-      "<input class='form-check-input form-check-input-checked-color-dark col-1 mt-2 ms-2' name='completed' type='checkbox' value = '' data-id='" + task.id + "' " + (task.completed ? "checked" : "") + ">" +
-      "<div class='col-8 task pt-1 ms-5 ps-5' data-id='" + task.id + "'>" +
-        task.content +
-      "</div>" + 
-      "<button class = 'btn btn-dark btn-sm remove col-2' data-id = '" + task.id + "'>remove</button>"
-  });
 
-  $("#tasks").html(htmlString);
-});
 
 var allTasks = function () {
   $(".task-list").each(function (i, ele) {
     $(this).show();
   });
-}
+};
 
 var activeTasks = function () {
   $(".task-list").each(function (i, ele) {
@@ -35,7 +24,7 @@ var activeTasks = function () {
       $(this).show();
     }
   });
-}
+};
 
 var completeTasks = function () {
   $(".task-list").each(function (i, ele) {
@@ -45,11 +34,10 @@ var completeTasks = function () {
       $(this).show();
     }
   });
-}
+};
 
+$(document).ready(function() {
 
-
-$(function() {
   $('#addTask').on('submit', function(event) {
     event.preventDefault();
     postTask($('#newToDo').val());
@@ -78,5 +66,7 @@ $(function() {
   $('.toggle-complete').on('click', function () {
     completeTasks();
   })
+
+  indexTasks();
 
 });
--- app/javascript/src/requests.js
+++ app/javascript/src/requests.js
@@ -6,18 +6,31 @@ $.ajaxSetup({
   }
 });
 
-export var indexTasks = function (successCB, errorCB) {
+export var indexTasks = function () {
   var request = {
     type: 'GET',
     url: 'api/tasks?api_key=1',
-    success: successCB,
-    error: errorCB
+    success: function (response) {
+      $("#tasks").empty();
+      response.tasks.map(function(task) {
+        $("#tasks").append("<div class='row mb-3 p-2 border rounded task-list'>" + 
+          "<input class='form-check-input form-check-input-checked-color-dark col-1 mt-2 ms-2' name='completed' type='checkbox' value = '' data-id='" + task.id + "' " + (task.completed ? "checked" : "") + ">" +
+          "<div class='col-8 task pt-1 ms-5 ps-5' data-id='" + task.id + "'>" +
+            task.content +
+          "</div>" + 
+          "<button class = 'btn btn-dark btn-sm remove col-2' data-id = '" + task.id + "'>remove</button>"
+        )
+      });
+    },
+    error: function (reqest, error) {
+      console.log(error)
+    }
   }
 
   $.ajax(request);
 };
 
-export var postTask = function (content, successCB, errorCB) {
+export var postTask = function (content) {
   var request = {
     type: 'POST',
     url: 'api/tasks?api_key=1',
@@ -27,21 +40,26 @@ export var postTask = function (content, successCB, errorCB) {
       }
     },
     success: function (response) {
-      successCB;
       indexTasks();
     },
-    error: errorCB
+    error: function (response, error) {
+      console.log(error);
+    }
   }
 
   $.ajax(request);
 };
 
-export var deleteTask = function (id, successCB, errorCB) {
+export var deleteTask = function (id) {
   var request = {
     type: 'DELETE',
     url: 'api/tasks/' + id + '?api_key=1',
-    success: successCB,
-    error: errorCB
+    success: function (response) {
+      indexTasks();
+    },
+    error: function (request, error) {
+      console.log(error);
+    }
   }
 
   $.ajax(request);
